fb237829863e75e149734e86b1fff3205ccb4ea3
nxhao <nxhao235@gmail.com>
2019-01-01 11:42:24 +0700

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,22 @@
-$(document).ready(function(){
-    $('.go-to-top').click(function() {
+$(document).ready(function () {
+    $('.go-to-top').click(function () {
         $(window.opera ? 'html' : 'html, body').animate({
             scrollTop: 0
         }, 'slow');
     });
+
+    /* Carousel */
     $('.carousel').carousel({
         interval: false
+    });
+
+    /* Search */
+    $('#searchButton').click(function () {
+        window.open("search.html", "_self");
+    })
+    $('#usersSearch').keypress(function (e) {
+        if (e.which == 13) {//Enter key pressed
+            $('#searchButton').click();//Trigger search button click event
+        }
     });
 });
